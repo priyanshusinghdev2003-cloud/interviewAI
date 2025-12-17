9c8362b29ad0a5f90a3c5a51240389792854c1e5
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2025-12-17 19:17:23 +0530

    Initialize Next.js project with Firebase authentication, core UI components, and utility functions.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,50 @@
+import InterviewCard from "@/components/InterviewCard";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 const page = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview Ready with AI-Powered Practise & Feedback</h2>
+          <p className="text-lg">
+            Practise on real interview questions & get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src={"/robot.png"}
+          alt="robot"
+          height={400}
+          width={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+          {/* <p className="text-lg">You haven&apos;t any interviews yet</p> */}
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+          {/* <p>There are no interviews available</p> */}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default page;
