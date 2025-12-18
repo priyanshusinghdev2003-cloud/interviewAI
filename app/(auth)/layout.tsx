5d2e84c7f0754b3e9990f6293d8aa295c75936c2
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2025-12-18 11:59:49 +0530

    Implement Firebase authentication actions and protect routes with dedicated layouts.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,13 @@
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-const layout = ({ children }: { children: ReactNode }) => {
+const layout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (isUserAuthenticated) {
+    return redirect("/");
+  }
   return <div className="auth-layout">{children}</div>;
 };
 
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
+import { isAuthenticated } from "@/lib/actions/auth.action";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function RootLayout({ children }: { children: React.ReactNode }) {
+async function RootLayout({ children }: { children: React.ReactNode }) {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) {
+    return redirect("/sign-in");
+  }
   return (
     <div className="root-layout">
       <nav className="">
